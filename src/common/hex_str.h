6f52c43f63ceb3470b35b288d5a6ebfb396a1cf4
who-biz <37732338+who-biz@users.noreply.github.com>
2019-06-27 00:03:37 -0400

    Remove bitcoin files from common directory

--- src/common/hex_str.h
+++ src/common/hex_str.h
@@ -1,5 +1,5 @@
 #include <string>
 #include <vector>
 
-  std::string bytes256_to_hex(const std::vector<unsigned char> input);
+  std::string bytes256_to_hex(std::vector<uint8_t>& input);
   std::vector<uint8_t> hex_to_bytes256(const std::string &input);
