00a6d3e95078168a9a33777abf944ca940b3e950
who-biz <37732338+who-biz@users.noreply.github.com>
2020-01-09 22:14:56 -0500

    Add copyright header to komodo_notaries.cpp

--- src/cryptonote_core/komodo_notaries.cpp
+++ src/cryptonote_core/komodo_notaries.cpp
@@ -1,3 +1,51 @@
+/******************************************************************************
+ * Copyright © 2014-2018 The SuperNET Developers.                     *
+ *                                                    *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at            *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                    *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * SuperNET software, including this file may be copied, modified, propagated *
+ * or distributed except according to the terms contained in the LICENSE file *
+ *                                                    *
+ * Removal or modification of this copyright notice is prohibited.        *
+ *                                                    *
+ ******************************************************************************/
+
+/****************************************************************************************
+ * Parts of this file have been modified for compatibility with the Blur Network.
+ * The copyright notice below applies to only those portions that have been changed.
+ *
+ * Copyright (c) Blur Network, 2018-2020
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ *    conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *    of conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ****************************************************************************************/
+
 #include "komodo_notaries.h"
 #include "komodo_notary_server/notary_server.h"
 #include "komodo/komodo_validation.h"
