cbb10c00f1ffdd44e149939d3e7c440fed87d6a9
who-biz <37732338+who-biz@users.noreply.github.com>
2020-02-03 07:47:33 -0500

    Add ntzindex on add_ntz_transaction_data to DB

--- src/blockchain_db/lmdb/db_lmdb.cpp
+++ src/blockchain_db/lmdb/db_lmdb.cpp
@@ -825,10 +825,12 @@ uint64_t BlockchainLMDB::add_ntz_transaction_data(const crypto::hash& blk_hash, 
   uint64_t m_height = height();
 
   int result;
-  uint64_t tx_id = get_notarization_count();
+  uint64_t tx_id = get_tx_count();
+  uint64_t ntz_id = get_notarization_count();
 
   CURSOR(ntz_txs)
   CURSOR(tx_indices)
+  CURSOR(ntz_indices)
 
   MDB_val_set(val_tx_id, tx_id);
   MDB_val_set(val_h, tx_hash);
@@ -838,6 +840,15 @@ uint64_t BlockchainLMDB::add_ntz_transaction_data(const crypto::hash& blk_hash, 
     throw1(TX_EXISTS(std::string("Attempting to add notarization transaction that's already in the db (tx id ").append(boost::lexical_cast<std::string>(tip->data.tx_id)).append(")").c_str()));
   } else if (result != MDB_NOTFOUND) {
     throw1(DB_ERROR(lmdb_error(std::string("Error checking if tx index exists for tx hash ") + epee::string_tools::pod_to_hex(tx_hash) + ": ", result).c_str()));
+  }
+
+  MDB_val_set(val_ntz_id, ntz_id);
+  result = mdb_cursor_get(m_cur_ntz_indices, (MDB_val *)&zerokval, &val_h, MDB_GET_BOTH);
+  if (result == 0) {
+    ntzindex *ni = (ntzindex *)val_h.mv_data;
+    throw1(TX_EXISTS(std::string("Attempting to add notarization index that's already in the db (ntz id ").append(boost::lexical_cast<std::string>(ni->key)).append(")").c_str()));
+  } else if (result != MDB_NOTFOUND) {
+    throw1(DB_ERROR(lmdb_error(std::string("Error checking if ntz index exists for ntz hash ") + epee::string_tools::pod_to_hex(tx_hash) + ": ", result).c_str()));
   }
 
   txindex ti;
@@ -852,6 +863,19 @@ uint64_t BlockchainLMDB::add_ntz_transaction_data(const crypto::hash& blk_hash, 
   result = mdb_cursor_put(m_cur_tx_indices, (MDB_val *)&zerokval, &val_h, 0);
   if (result)
     throw0(DB_ERROR(lmdb_error("Failed to add ntz tx data to db transaction: ", result).c_str()));
+
+
+  ntzindex ntzind;
+  ntzind.key = tx_hash;
+  ntzind.ntz_id = tx_id;
+  ntzind.ntz_height = m_height;  // we don't need blk_hash since we know m_height
+
+  val_h.mv_size = sizeof(ntzind);
+  val_h.mv_data = (void *)&ntzind;
+
+  result = mdb_cursor_put(m_cur_ntz_indices, (MDB_val *)&zerokval, &val_h, 0);
+  if (result)
+    throw0(DB_ERROR(lmdb_error("Failed to add ntz index for db transaction: ", result).c_str()));
 
   MDB_val_copy<blobdata> blob(tx_to_blob(tx));
   result = mdb_cursor_put(m_cur_ntz_txs, &val_tx_id, &blob, MDB_APPEND);
