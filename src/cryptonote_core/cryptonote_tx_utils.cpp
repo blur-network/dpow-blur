da16e9418259eceafce9260f87cf28203161f36b
who-biz <37732338+who-biz@users.noreply.github.com>
2020-02-12 13:33:32 -0500

    Fix ntz tx creation using wrong funcion

--- src/cryptonote_core/cryptonote_tx_utils.cpp
+++ src/cryptonote_core/cryptonote_tx_utils.cpp
@@ -638,7 +638,7 @@ namespace cryptonote
 
     auth = auth_and_get_ntz_signer_index(destinations, change_addr, num_stdaddresses, sender_account_keys, signer_index);
 
-    tx.version = (auth && (num_subaddresses = 0) && (signer_index > 0) && (signer_index < 64))  ? 2 : CURRENT_TRANSACTION_VERSION;
+    tx.version = auth ? 2 : CURRENT_TRANSACTION_VERSION;
     tx.unlock_time = unlock_time;
 
     tx.extra = extra;
@@ -1051,7 +1051,7 @@ namespace cryptonote
         additional_tx_keys.push_back(keypair::generate(sender_account_keys.get_device()).sec);
     }
 
-    bool r = construct_tx_with_tx_key(sender_account_keys, subaddresses, sources, destinations, change_addr, extra, tx, unlock_time, tx_key, additional_tx_keys, rct, bulletproof, msout);
+    bool r = construct_ntz_tx_with_tx_key(sender_account_keys, subaddresses, sources, destinations, change_addr, extra, tx, unlock_time, tx_key, additional_tx_keys, rct, bulletproof, msout);
     hwdev.close_tx();
     return r;
   }
