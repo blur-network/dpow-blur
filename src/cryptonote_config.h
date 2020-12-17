de76316c2208fc131dc904ef7dd76218f4cdba98
who-biz <37732338+who-biz@users.noreply.github.com>
2020-12-17 23:47:49 +0000

    Increase DPOW_SIG_COUNT to 7

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -36,7 +36,7 @@
 #include <string>
 #include <boost/uuid/uuid.hpp>
 
-#define DPOW_SIG_COUNT                                  5
+#define DPOW_SIG_COUNT                                  7
 #define DPOW_NOTARIZATION_WINDOW			26
 #define CRYPTONOTE_MAX_BLOCK_NUMBER                     500000000
 #define CRYPTONOTE_MAX_BLOCK_SIZE                       500000000  /* block header blob limit, never used! */
