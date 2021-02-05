108700fb46ed868382218fc0577ac7bb8bbf8fa6
who-biz <37732338+who-biz@users.noreply.github.com>
2021-02-05 22:38:20 +0000

    Check txpool if tx is not found in DB, in handle_block_to_*_chain

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1647,6 +1647,15 @@ bool Blockchain::handle_alternative_block(const block& b, const crypto::hash& id
         std::vector<cryptonote::blobdata> txs_blobs;
         std::vector<crypto::hash> missed_ids;
         get_transactions_blobs(bei.bl.tx_hashes, txs_blobs, missed_ids);
+        if (missed_ids.size())  {
+          //check txpool
+          for (const auto& each : missed_ids) {
+            cryptonote::blobdata each_blob;
+            if (!get_txpool_tx_blob(each, each_blob)) {
+              MERROR("Failed to get transaction from both database and txpool for id: " << each << ", in handle_alternative");
+            }
+          }
+        }
         // TODO: May need to handle missed_ids here and request those txs from other nodes
         for (const auto& txblob: txs_blobs)
         {
@@ -3938,6 +3947,15 @@ leave:
         std::vector<cryptonote::blobdata> txs_blobs;
         std::vector<crypto::hash> missed_ids;
         get_transactions_blobs(bl.tx_hashes, txs_blobs, missed_ids);
+        if (missed_ids.size())  {
+          //check txpool
+          for (const auto& each : missed_ids) {
+            cryptonote::blobdata each_blob;
+            if (!get_txpool_tx_blob(each, each_blob)) {
+              MERROR("Failed to get transaction from both database and txpool for id: " << each << ", in handle_to_main_chain");
+            }
+          }
+        }
         // TODO: May need to handle missed_ids here and request those txs from other nodes
         for (const auto& txblob: txs_blobs)
         {
