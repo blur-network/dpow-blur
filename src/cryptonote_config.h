d2f090e488ecf2677b36fa89dbee225349f11945
who-biz <37732338+who-biz@users.noreply.github.com>
2021-02-11 18:25:05 -0500

    Set MAX_NOTA_PER_BLOCK = DPOW_SIG_COUNT [breaking]
    - This change will break the current code, but is needed for the changes that will follow
    - Currently there is only the final nota tx in a nota sequence included in a block. This is not the desired behavior
    - Changes to follow will convert entire ntzpool to txpool, so that end result will be an 'effective msig' (not a true multisig tx)

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -37,10 +37,10 @@
 #include <boost/uuid/uuid.hpp>
 
 #define DPOW_SIG_COUNT                                  5
-#define DPOW_MAX_NOTA_PER_BLOCK				1
-#define DPOW_FORK_VERSION				11
-#define DPOW_NOTA_TX_VERSION				2
-#define DPOW_NOTARIZATION_WINDOW			25
+#define DPOW_MAX_NOTA_PER_BLOCK                         (DPOW_SIG_COUNT)
+#define DPOW_FORK_VERSION                               11
+#define DPOW_NOTA_TX_VERSION                            2
+#define DPOW_NOTARIZATION_WINDOW                        25
 
 #define CRYPTONOTE_MAX_BLOCK_NUMBER                     500000000
 #define CRYPTONOTE_MAX_BLOCK_SIZE                       500000000  /* block header blob limit, never used! */
