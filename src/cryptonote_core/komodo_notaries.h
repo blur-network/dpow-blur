449f0a46be5568311554f3a6e41bcb53b8bffd91
who-biz <37732338+who-biz@users.noreply.github.com>
2021-03-09 20:51:16 +0000

    Add Base58 encoding/decoding functions to komodo_notaries.cpp

--- src/cryptonote_core/komodo_notaries.h
+++ src/cryptonote_core/komodo_notaries.h
@@ -60,6 +60,47 @@
 struct sha256_vstate { uint64_t length; uint32_t state[8],curlen; uint8_t buf[64]; };
 
 namespace cryptonote {
+/**
+ * Encode a byte sequence as a base58-encoded string.
+ * pbegin and pend cannot be nullptr, unless both are.
+ */
+  std::string EncodeBase58(const unsigned char* pbegin, const unsigned char* pend);
+
+/**
+ * Encode a byte vector as a base58-encoded string
+ */
+  std::string EncodeBase58(const std::vector<unsigned char>& vch);
+
+/**
+ * Decode a base58-encoded string (psz) into a byte vector (vchRet).
+ * return true if decoding is successful.
+ * psz cannot be nullptr.
+ */
+  bool DecodeBase58(const char* psz, std::vector<unsigned char>& vchRet);
+
+/**
+ * Decode a base58-encoded string (str) into a byte vector (vchRet).
+ * return true if decoding is successful.
+ */
+  bool DecodeBase58(const std::string& str, std::vector<unsigned char>& vchRet);
+
+/**
+ * Encode a byte vector into a base58-encoded string, including checksum
+ */
+std::string EncodeBase58Check(const std::vector<unsigned char>& vchIn);
+
+/**
+ * Decode a base58-encoded string (psz) that includes a checksum into a byte
+ * vector (vchRet), return true if decoding is successful
+ */
+  bool DecodeBase58Check(const char* psz, std::vector<unsigned char>& vchRet);
+
+/**
+ * Decode a base58-encoded string (str) that includes a checksum into a byte
+ * vector (vchRet), return true if decoding is successful
+ */
+  bool DecodeBase58Check(const std::string& str, std::vector<unsigned char>& vchRet);
+
   bool get_notary_pubkeys(std::vector<std::pair<crypto::public_key,crypto::public_key>>& notary_pubkeys);
   bool get_notary_secret_viewkeys(std::vector<crypto::secret_key>& notary_viewkeys);
 
