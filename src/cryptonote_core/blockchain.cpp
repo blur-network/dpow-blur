efccf2c21ed3c2a0778b70c851f3e8cd9178ba9a
who-biz <37732338+who-biz@users.noreply.github.com>
2021-06-15 23:20:17 +0000

    [untested] Add consensus rule for checking btc_tx_data in DB
    - Specifically block height at which it was embedded

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1718,6 +1718,17 @@ bool Blockchain::handle_alternative_block(const block& b, const crypto::hash& id
           bvc.m_verifivation_failed = true;
           return false;
         }
+        else
+        {
+          for (const auto& each : btc_hashes)
+          {
+            crypto::hash btc_hash = crypto::null_hash;
+            if (!string_to_hash(each, btc_hash))
+            {
+              
+            }
+          }
+        }
       }
       else
       {
@@ -4091,6 +4102,26 @@ leave:
               }
               bvc.m_verifivation_failed = true;
               return false;
+            }
+            else
+            {
+              for (const auto& each : btc_hashes)
+              {
+                crypto::hash btc_hash = crypto::null_hash;
+                if (!string_to_hash(each, btc_hash))
+                {
+                  if (m_db->btc_tx_exists(btc_hash))
+                  {
+                    uint64_t btcindex_height = m_db->get_btc_tx_block_height(btc_hash);
+                    if (btcindex_height != m_db->height())
+                    {
+                      MERROR("Attempting to add duplicate btc_tx_data at a subsequent height!");
+                      bvc.m_verifivation_failed = true;
+                      return false;
+                    }
+                  }
+                }
+              }
             }
           }
           MWARNING("Notarizing block at height: " << std::to_string(get_block_height(bl)) << ", notarization tx count: " << std::to_string(num_ntz_txs));
