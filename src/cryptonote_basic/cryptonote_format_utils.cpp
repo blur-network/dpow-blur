78a7d9e61adfd863522adb12f99dedb11b941306
who-biz <37732338+who-biz@users.noreply.github.com>
2021-10-28 19:11:53 +0000

    Update copyright info for 2022

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2021, The Blur Network
+// Copyright (c) 2018-2022, The Blur Network
 // Copyright (c) 2017-2018, The Masari Project
 // Copyright (c) 2014-2018, The Monero Project
 //
