blur-network/dpow-blur

commit f33fc91969625649be4d3caac4c22b285bf63fe7

Add new RPCs from blur core to API
- Also some miscellaneous housekeeping

src/p2p/net_node.inl
@@ -86,7 +86,46 @@ namespace nodetool
   {
 
     TRY_ENTRY();
-    make_default_config();
+    std::string state_file_path = m_config_folder + "/" + P2P_NET_DATA_FILENAME;
+    std::ifstream p2p_data;
+    p2p_data.open( state_file_path , std::ios_base::binary | std::ios_base::in);
+    if(!p2p_data.fail())
+    {
+      try
+      {
+        // first try reading in portable mode
+        boost::archive::portable_binary_iarchive a(p2p_data);
+        a >> *this;
+      }
+      catch (...)
+      {
+        // if failed, try reading in unportable mode
+        boost::filesystem::copy_file(state_file_path, state_file_path + ".unportable", boost::filesystem::copy_option::overwrite_if_exists);
+        p2p_data.close();
+        p2p_data.open( state_file_path , std::ios_base::binary | std::ios_base::in);
+        if(!p2p_data.fail())
+        {
+          try
+          {
+            boost::archive::binary_iarchive a(p2p_data);
+            a >> *this;
+          }
+          catch (const std::exception &e)
+          {
+            MWARNING("Failed to load p2p config file, falling back to default config");
+            m_peerlist = peerlist_manager(); // it was probably half clobbered by the failed load
+            make_default_config();
+          }
+        }
+        else
+        {
+          make_default_config();
+        }
+      }
+    }else
+    {
+      make_default_config();
+    }
 
     // always recreate a new peer id
     make_default_peer_id();
@@ -169,7 +208,7 @@ namespace nodetool
     for (const auto &c: conns)
       m_net_server.get_config_object().close(c);
 
-    MCLOG_CYAN(el::Level::Info, "global", "Host " << addr.host_str() << " blocked.");
+    MCLOG_CYAN(el::Level::Info, "global", "Host " << addr.host_str() << " blocked due to host being found on block list.");
     return true;
   }
   //-----------------------------------------------------------------------------------
@@ -195,8 +234,10 @@ namespace nodetool
     {
       auto it = m_host_fails_score.find(address.host_str());
       CHECK_AND_ASSERT_MES(it != m_host_fails_score.end(), false, "internal error");
-      if ((it->second) >= 9)
+      if ((it->second) >= 9) {
         block_host(address);
+        MCLOG_CYAN(el::Level::Info, "global", "Host " << address.host_str() << " added to block list. Failed to connect after at least 9 attempts."); 
+      }
     }
     return true;
   }
@@ -426,6 +467,20 @@ namespace nodetool
     res = m_peerlist.init(m_allow_local_ip);
     CHECK_AND_ASSERT_MES(res, false, "Failed to init peerlist.");
 
+    // if we are on mainnet, and have neither exclusive nor priority nodes specified
+    if ((m_nettype == cryptonote::MAINNET) && ((m_priority_peers.size() == 0) && (m_exclusive_peers.size() == 0))) {
+      std::vector<std::string> seed_peers = SEED_NODE_PEERS;
+      for (const auto& each: seed_peers) {
+        nodetool::peerlist_entry pe = AUTO_VAL_INIT(pe);
+        pe.id = crypto::rand<uint64_t>();
+        const uint16_t default_port = cryptonote::get_config(m_nettype).P2P_DEFAULT_PORT;
+        bool r = parse_peer_from_string(pe.adr, each, default_port);
+        if (r)
+        {
+          m_command_line_peers.push_back(pe);
+        }
+      }
+    }
 
     for(auto& p: m_command_line_peers)
       m_peerlist.append_with_peer_white(p);
@@ -1105,22 +1160,23 @@ namespace nodetool
     {
       if(conn_count < expected_white_connections)
       {
-        //start from anchor list
-        if(!make_expected_connections_count(anchor, P2P_DEFAULT_ANCHOR_CONNECTIONS_COUNT))
-          return false;
-        //then do white list
+        //start from white list
         if(!make_expected_connections_count(white, expected_white_connections))
+          return false;
+        //then do anchor list
+        if(!make_expected_connections_count(anchor, P2P_DEFAULT_ANCHOR_CONNECTIONS_COUNT))
           return false;
         //then do grey list
         if(!make_expected_connections_count(gray, m_config.m_net_config.max_out_connection_count))
           return false;
-      }else
+      }
+      else
       {
-        //start from grey list
-        if(!make_expected_connections_count(gray, m_config.m_net_config.max_out_connection_count))
-          return false;
-        //and then do white list
+        //start from white list
         if(!make_expected_connections_count(white, m_config.m_net_config.max_out_connection_count))
+          return false;
+        //then do grey list
+        if(!make_expected_connections_count(gray, m_config.m_net_config.max_out_connection_count))
           return false;
       }
     }
@@ -1154,11 +1210,11 @@ namespace nodetool
       if(m_net_server.is_stop_signal_sent())
         return false;
 
-      if (peer_type == anchor && !make_new_connection_from_anchor_peerlist(apl)) {
+      if (peer_type == white && !make_new_connection_from_peerlist(true)) {
         break;
       }
 
-      if (peer_type == white && !make_new_connection_from_peerlist(true)) {
+      if (peer_type == anchor && !make_new_connection_from_anchor_peerlist(apl)) {
         break;
       }
 
@@ -1670,7 +1726,7 @@ namespace nodetool
   bool node_server<t_payload_net_handler>::set_max_out_peers(const boost::program_options::variables_map& vm, int64_t max)
   {
     if(max == -1) {
-      m_config.m_net_config.max_out_connection_count = -1;
+      m_config.m_net_config.max_out_connection_count = 9999;
       return true;
     }
     m_config.m_net_config.max_out_connection_count = max;

src/gen_multisig/CMakeLists.txt
@@ -31,23 +31,23 @@ set(gen_multisig_sources
 
 monero_add_executable(gen_multisig
   ${gen_multisig_sources})
+add_dependencies(gen_multisig multisig wallet)
 target_link_libraries(gen_multisig
   PRIVATE
     wallet
     cryptonote_core
-    cryptonote_protocol
     cncrypto
     common
     epee
     ${EPEE_READLINE}
+    ${READLINE_LIBRARIES} 
     ${Boost_CHRONO_LIBRARY}
     ${Boost_PROGRAM_OPTIONS_LIBRARY}
     ${Boost_FILESYSTEM_LIBRARY}
     ${Boost_THREAD_LIBRARY}
     ${CMAKE_THREAD_LIBS_INIT}
-    ${READLINE_LIBRARIES}
-    ${EXTRA_LIBRARIES}
-    ${OPENSSL_LIBRARIES})
+    ${OPENSSL_LIBRARIES}
+    ${EXTRA_LIBRARIES})
 set_property(TARGET gen_multisig
   PROPERTY
     OUTPUT_NAME "blur-gen-trusted-multisig")

src/gen_multisig/gen_multisig.cpp
@@ -38,7 +38,7 @@
 #include <boost/program_options.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/format.hpp>
-#include "include_base_utils.h"
+#include "misc_log_ex.h"
 #include "crypto/crypto.h"  // for crypto::secret_key definition
 #include "common/i18n.h"
 #include "common/command_line.h"

src/multisig/CMakeLists.txt
@@ -42,6 +42,7 @@ monero_add_library(multisig
   ${multisig_headers}
   ${multisig_private_headers})
 
+
 target_link_libraries(multisig
   PUBLIC
     ringct

src/multisig/multisig.cpp
@@ -27,7 +27,7 @@
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <unordered_set>
-#include "include_base_utils.h"
+#include "misc_log_ex.h"
 #include "crypto/crypto.h"
 #include "ringct/rctOps.h"
 #include "cryptonote_basic/account.h"
@@ -36,8 +36,6 @@
 
 #undef MONERO_DEFAULT_LOG_CATEGORY
 #define MONERO_DEFAULT_LOG_CATEGORY "multisig"
-
-using namespace std;
 
 static const rct::key multisig_salt = { {'M', 'u', 'l', 't' , 'i', 's', 'i', 'g', 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 } };
 

src/p2p/net_node.cpp
@@ -45,8 +45,8 @@ namespace nodetool
     const command_line::arg_descriptor<std::vector<std::string> > arg_p2p_seed_node   = {"seed-node", "Connect to a node to retrieve peer addresses, and disconnect"};
     const command_line::arg_descriptor<bool> arg_p2p_hide_my_port   =    {"hide-my-port", "Do not announce yourself as peerlist candidate", false, true};
 
-    const command_line::arg_descriptor<int64_t>     arg_out_peers = {"out-peers", "set max number of out peers", -1};
-    const command_line::arg_descriptor<int64_t>     arg_in_peers = {"in-peers", "set max number of in peers", -1};
+    const command_line::arg_descriptor<int64_t>     arg_out_peers = {"out-peers", "set max number of out peers", 32};
+    const command_line::arg_descriptor<int64_t>     arg_in_peers = {"in-peers", "set max number of in peers", 8};
     const command_line::arg_descriptor<int> arg_tos_flag = {"tos-flag", "set TOS flag", -1};
 
     const command_line::arg_descriptor<int64_t> arg_limit_rate_up = {"limit-rate-up", "set limit-rate-up [kB/s]", 4096};

src/rpc/CMakeLists.txt
@@ -95,7 +95,6 @@ target_link_libraries(rpc_base
   PUBLIC
     common
     epee
-    cryptonote_protocol
     ${Boost_REGEX_LIBRARY}
     ${Boost_THREAD_LIBRARY}
     ${Boost_PROGRAM_OPTIONS_LIBRARY}

src/rpc/core_rpc_server.cpp
@@ -30,10 +30,9 @@
 //
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
 
-#include "include_base_utils.h"
+#include "misc_log_ex.h"
 #include "string_tools.h"
-#include "span.h"
-
+#include "string_coding.h"
 using namespace epee;
 
 #include "core_rpc_server.h"
@@ -237,7 +236,6 @@ namespace cryptonote
       boost::shared_lock<boost::shared_mutex> lock(m_bootstrap_daemon_mutex);
       res.was_bootstrap_ever_used = m_was_bootstrap_ever_used;
     }
-    res.version = MONERO_VERSION;
     return true;
   }
   //-----------------------------------------------------------------------------------------------------------------
@@ -275,6 +273,9 @@ namespace cryptonote
   bool core_rpc_server::on_get_blocks(const COMMAND_RPC_GET_BLOCKS_FAST::request& req, COMMAND_RPC_GET_BLOCKS_FAST::response& res)
   {
     PERF_TIMER(on_get_blocks);
+    bool r;
+    if (use_bootstrap_daemon_if_necessary<COMMAND_RPC_GET_BLOCKS_FAST>(invoke_http_mode::BIN, "/getblocks.bin", req, res, r))
+      return r;
 
     std::list<std::pair<cryptonote::blobdata, std::list<cryptonote::blobdata> > > bs;
 
@@ -2141,6 +2142,7 @@ namespace cryptonote
     response.block_size = m_core.get_blockchain_storage().get_db().get_block_size(height);
     response.num_txes = blk.tx_hashes.size();
     response.pow_hash = string_tools::pod_to_hex(get_block_longhash(blk, height));
+    response.tx_prefix_hash = string_tools::pod_to_hex(get_transaction_prefix_hash(blk.miner_tx));
     return true;
   }
   //------------------------------------------------------------------------------------------------------------------------------
@@ -2494,7 +2496,6 @@ namespace cryptonote
       boost::shared_lock<boost::shared_mutex> lock(m_bootstrap_daemon_mutex);
       res.was_bootstrap_ever_used = m_was_bootstrap_ever_used;
     }
-    res.version = MONERO_VERSION;
     return true;
   }
   //------------------------------------------------------------------------------------------------------------------------------
@@ -2805,6 +2806,29 @@ namespace cryptonote
     return true;
   }*/
   //------------------------------------------------------------------------------------------------------------------------------
+  bool core_rpc_server::on_base64_encode(const COMMAND_RPC_BASE64_ENCODE::request& req, COMMAND_RPC_BASE64_ENCODE::response& res, epee::json_rpc::error& error_resp)
+  {
+
+    unsigned char const bytes = req.bytes;
+
+    if (sizeof(bytes) < 1) {
+      res.status = "Minimum length of 1 byte needed to encode to base 64";
+      return false;
+    }
+
+    std::string base_64_string = epee::string_encoding::base64_encode(&bytes, sizeof(bytes));
+    res.base_64_string = base_64_string;
+
+    bool r = base_64_string.empty();
+    if (r) {
+      res.status = "Error encoding to base64 string";
+      return false;
+    }
+
+    res.status = CORE_RPC_STATUS_OK;
+    return true;
+  }
+  //------------------------------------------------------------------------------------------------------------------------------
   bool core_rpc_server::on_flush_txpool(const COMMAND_RPC_FLUSH_TRANSACTION_POOL::request& req, COMMAND_RPC_FLUSH_TRANSACTION_POOL::response& res, epee::json_rpc::error& error_resp)
   {
     PERF_TIMER(on_flush_txpool);
@@ -2843,8 +2867,8 @@ namespace cryptonote
     }
     if (!m_core.get_blockchain_storage().flush_txes_from_pool(txids))
     {
-        res.status = "Failed to remove one or more tx(es)";
-        return false;
+      res.status = "Failed to remove one or more tx(es)";
+      return false;
     }
 
     if (failed)

commit 23c816064c3201a10e23b524f6253f0fef977078

Fix compilation error introduced in f33fc91969625649be4d3caac4c22b285bf63fe7

src/p2p/net_node.inl
@@ -468,7 +468,7 @@ namespace nodetool
     CHECK_AND_ASSERT_MES(res, false, "Failed to init peerlist.");
 
     // if we are on mainnet, and have neither exclusive nor priority nodes specified
-    if ((m_nettype == cryptonote::MAINNET) && ((m_priority_peers.size() == 0) && (m_exclusive_peers.size() == 0))) {
+    /*if ((m_nettype == cryptonote::MAINNET) && ((m_priority_peers.size() == 0) && (m_exclusive_peers.size() == 0))) {
       std::vector<std::string> seed_peers = SEED_NODE_PEERS;
       for (const auto& each: seed_peers) {
         nodetool::peerlist_entry pe = AUTO_VAL_INIT(pe);
@@ -480,7 +480,7 @@ namespace nodetool
           m_command_line_peers.push_back(pe);
         }
       }
-    }
+    }*/
 
     for(auto& p: m_command_line_peers)
       m_peerlist.append_with_peer_white(p);