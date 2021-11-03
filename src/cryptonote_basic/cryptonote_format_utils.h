54e9e327599efd0bcf84a30e5c9bca2662a36b51
who-biz <37732338+who-biz@users.noreply.github.com>
2021-11-03 15:37:04 -0400

    Add note about missing ntzpool mdb_drop in DB->reset()
    - Also move 'remove_ntz_data_from_tx_extra' into congruent spot in cn_format_utils.h header

--- src/cryptonote_basic/cryptonote_format_utils.h
+++ src/cryptonote_basic/cryptonote_format_utils.h
@@ -83,7 +83,6 @@ namespace cryptonote
   bool add_tx_pub_key_to_extra(std::vector<uint8_t>& tx_extra, const crypto::public_key& tx_pub_key);
   bool add_ntz_signer_index_to_extra(std::vector<uint8_t>& tx_extra, const uint8_t& idx);
   bool add_ntz_txn_to_extra(std::vector<uint8_t>& tx_extra, const std::string& input);
-  void remove_ntz_data_from_tx_extra(std::vector<uint8_t> const& tx_extra, std::vector<uint8_t>& new_extra, std::vector<uint8_t>& ntz_data, blobdata& ntz_str, int& signer_index);
   bool verify_embedded_ntz_data(cryptonote::transaction const& tx, crypto::hash& btc_hash, uint64_t& height, int& ntz_signer_idx);
   int32_t verify_embedded_ntz_data(std::vector<cryptonote::transaction> const& txs, std::vector<std::string>& btc_hashes, std::vector<uint64_t>& heights, std::vector<uint32_t>& inferior_idxs);
   bool check_signer_index_with_viewkeys(cryptonote::transaction const& tx);
@@ -106,6 +105,7 @@ namespace cryptonote
   bool lookup_acc_outs(const account_keys& acc, const transaction& tx, const crypto::public_key& tx_pub_key, const std::vector<crypto::public_key>& additional_tx_public_keys, std::vector<size_t>& outs, uint64_t& money_transfered);
   bool lookup_acc_outs(const account_keys& acc, const transaction& tx, std::vector<size_t>& outs, uint64_t& money_transfered);
   bool get_tx_fee(const transaction& tx, uint64_t & fee);
+  void remove_ntz_data_from_tx_extra(std::vector<uint8_t> const& tx_extra, std::vector<uint8_t>& new_extra, std::vector<uint8_t>& ntz_data, blobdata& ntz_str, int& signer_index);
   uint64_t get_tx_fee(const transaction& tx);
   bool generate_key_image_helper(const account_keys& ack, const std::unordered_map<crypto::public_key, subaddress_index>& subaddresses, const crypto::public_key& out_key, const crypto::public_key& tx_public_key, const std::vector<crypto::public_key>& additional_tx_public_keys, size_t real_output_index, keypair& in_ephemeral, crypto::key_image& ki, hw::device &hwdev);
   bool generate_key_image_helper_precomp(const account_keys& ack, const crypto::public_key& out_key, const crypto::key_derivation& recv_derivation, size_t real_output_index, const subaddress_index& received_index, keypair& in_ephemeral, crypto::key_image& ki, hw::device &hwdev);
