1ca491c90b68ff1fd3fdeb40202409aed7b7e145
who-biz <37732338+who-biz@users.noreply.github.com>
2023-09-06 13:43:33 -0400

    Increase minsigs to 5 for testnet

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -36,7 +36,7 @@
 #include <string>
 #include <boost/uuid/uuid.hpp>
 
-#define DPOW_SIG_COUNT                                  2
+#define DPOW_SIG_COUNT                                  5
 #define DPOW_MAX_NOTA_PER_BLOCK                         (DPOW_SIG_COUNT)
 #define DPOW_FORK_VERSION                               11
 #define DPOW_NOTA_TX_VERSION                            2
